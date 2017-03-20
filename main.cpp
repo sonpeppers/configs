5447f486687d27644c324534f51c421a87458fdf
Paul Epperson (peppers) <pepperson00@gmail.com>
2017-03-20 01:36:54 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Paul Epperson (github.com/sonpeppers)
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+  cout << "Running \"" << argv[0] << "\" . . ." << endl;
+
+  // TODO
+
+  return 0;
+}
