8d857c1308c3a04c9a297a178a1b327dbf90daff
Paul Epperson (peppers) <pepperson00@gmail.com>
2017-03-22 17:46:56 -0700

    Update boot_install.sh

--- boot_install.sh
+++ boot_install.sh
@@ -66,9 +66,10 @@ fi
 sudo apt-get --assume-yes update
 
 # setup bare bone essentials
-sudo apt-get --assume-yes install vim-gnome git
-sudo apt-get --assume-yes install zsh csh ksh ash curl
-sudo apt-get --assume-yes install samba samba-common python-glade2 system-config-samba
+sudo apt-get --assume-yes install vim-gnome git gitk
+sudo apt-get --assume-yes install zsh csh ksh ash curl expect unzip wget
+sudo apt-get --assume-yes install build-essential cmake autotools-dev pkg-config
+printf "\n\n\n" | ssh-keygen -t rsa
 
 # takeout online search
 echo '[Desktop Entry]' >> disable_onlinesearch.desktop
@@ -112,11 +113,16 @@ chmod 644 setBackground.sh
 sudo mv setBackground.sh /etc/profile.d
 
 # pip, anaconda, jupyter
-sudo apt-get --assume-yes install python3-pip
+sudo apt-get --assume-yes install python-pip python3-pip
 wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
 sudo bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p '/opt/anaconda3'
 rm Anaconda-4.3.1-Linux-x86_64.sh
 pip3 install jupyter
+
+# atom
+wget https://atom.io/download/deb
+sudo dpkg --install deb
+rm deb
 
 # node, express, angular, mongodb
 sudo apt-get --assume-yes install npm
@@ -126,10 +132,13 @@ sudo npm install express -g
 sudo npm install angular -g
 curl https://install.meteor.com | /bin/sh
 
-# java, eclipse
+# java, eclipse, ant
 # Note: use sudo update-alternatives --config javac to choose between versions
 #       and set JAVA_HOME in /etc/environment to the path of the version chosen
-sudo apt-get --assume-yes install default-jdk eclipse
+sudo apt-get --assume-yes install default-jdk eclipse ant
+
+# doxygen
+sudo apt-get --assume-yes install doxygen
 
 # google chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -161,12 +170,7 @@ sudo apt-get --assume-yes install python-rosinstall
 # arduino
 sudo apt-get --assume-yes install arduino arduino-core
 
-# atom
-wget https://atom.io/download/deb
-sudo dpkg --install deb
-rm deb
-
-# ardupilot's apmplanner
+# apmplanner
 wget http://firmware.us.ardupilot.org/Tools/APMPlanner/apm_planner_2.0.24_xenial64.deb
 sudo dpkg -i apm_planner_2.0.24_xenial64.deb
 sudo apt-get -f --assume-yes install
@@ -189,11 +193,37 @@ mkdir -p ~/devel/eagle/cam \
 # quartus
 # TODO
 
-# Qt
-sudo apt-get --assume-yes install qt5-default qtdeclarative5-dev
+# Qt, GTK3
+sudo apt-get --assume-yes install qt5-default qtdeclarative5-dev qtcreator
+sudo apt-get --assume-yes install libgtk-3-dev
 
-# tesseract, 0MQ (?)
+# tesseract
 sudo apt-get --assume-yes install tesseract-ocr
+
+# OpenCV (shotgun approach)
+# see http://www.pyimagesearch.com/2016/10/24/ubuntu-16-04-how-to-install-opencv/
+# see https://github.com/milq/milq/blob/master/scripts/bash/install-opencv.sh
+sudo apt-get --assume-yes install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get --assume-yes install lbavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+sudo apt-get --assume-yes install libgtk-3-dev libatlas-base-dev gfortran
+sudo apt-get --assume-yes install libgtkglext1-dev libvtk6-dev zlib1g-dev libjpeg-dev libwebp-dev libpng-dev \
+	     			  libopenexr-dev libgdal-dev libdc1394-22-dev libtheora-dev libvorbis-dev \
+				  libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev \
+				  libopencore-amrwb-dev libv4l-dev libxine2-dev \
+				  libtbb-dev libeigen3-dev
+wget https://github.com/opencv/opencv/archive/3.2.0.zip
+unzip 3.2.0.zip
+rm 3.2.0.zip
+sudo mv opencv-3.2.0 /opt/OpenCV
+sudo mkdir /opt/OpenCV/build
+pushd /opt/OpenCV/build
+sudo cmake -DWITH_QT=ON -DWITH_OPENGL=ON -DFORCE_VTK=ON -DWITH_TBB=ON -DWITH_GDAL=ON -DWITH_XINE=ON -DBUILD_EXAMPLES=ON ..
+sudo make -j4
+sudo make install
+sudo ldconfig
+popd
+
+# 0MQ
 sudo apt-get --assume-yes install libtool pkg-config build-essential autoconf uuid-dev e2fsprogs
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.gz
 tar -xvf zeromq-4.2.1.tar.gz
@@ -206,12 +236,20 @@ sudo make install
 popd
 sudo ldconfig
 
-# miscellaneous
-sudo apt-get --assume-yes install gparted virtualbox
-sudo apt-get --assume-yes install octave
-sudo apt-get --assume-yes install gimp audacity
-sudo apt-get --assume-yes install shutter recordmydesktop gtk-recordmydesktop
-#skype
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+# samba
+sudo apt-get --assume-yes install samba samba-common python-glade2 system-config-samba
+
+# skype
+sudo --assume-yes add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
 sudo dpkg --add-architecture i386
-sudo apt-get update && sudo apt-get install skype
+sudo apt-get --assume-yes update && sudo apt-get --assume-yes install skype
+
+# miscellaneous
+sudo apt-get --assume-yes install gparted virtualbox \
+	     			  octave \
+				  gimp audacity \
+				  shutter recordmydesktop gtk-recordmydesktop
+
+##########
+# clean up
+sudo apt-get -assuem-yes autoremove
